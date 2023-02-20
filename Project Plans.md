92a93cf52551267a91839a8347b7b2c23910ab2c
Chimaezss <cheesewithease@outlook.com>
2023-02-20 11:58:14 -0500

    changed project plan, price tool updated with config file for prices

--- Project Plans.md
+++ Project Plans.md
@@ -12,3 +12,6 @@ Next steps:
     enter cards from set (possible find set?)
     get market value of total set
     check market value of card
+
+2/19 - Snipe Tool has little functionality, realized that there isn't much to that. The "Lazy Buy" tool is where I want to shift a majority of my focus.
+The goal is to have this done by madden 24 for a possible comeback. Maybe a snipe tool for MLB The Show would be more useful?
--- auctionTool.py
+++ auctionTool.py
@@ -1,40 +1,94 @@
-# Instructions for tool usage
-print('1) Snipe Tool')
-print('2) "Lazy Buy" Tool')
-print('3) Set Tool')
-print('Enter 1, 2, or 3')
+from pathlib import Path
 
-chosenMode = input()
-enteredPrice = 0
-if chosenMode == '1':
-    print('Enter price of card(-1 to quit): ')
-    enteredPrice = input()
-    while enteredPrice != '-1':
-        print('Enter desired profit: ')
-        desiredProfit = input()
-        buyPrice = (int(enteredPrice) * .90) - int(desiredProfit)
-        print(f'You should buy at {buyPrice} for a profit of {desiredProfit}.')
-        print('Enter price of card(-1 to quit): ')
-        enteredPrice = input()
-elif chosenMode == '2':
-    print('What is the overall range? ')
-    overallRange = input()
-    print(f'What is the "Lazy" price for {overallRange}\'s?')
-    enteredPrice = input()
-    buyPrice = int(enteredPrice) * .60
-    print(f'You should buy {overallRange}\'s for {buyPrice}.')
-elif chosenMode == '3':
-    allCardPrices = []
-    totalSetCost = 0
-    while enteredPrice != '-1':
-        print('Enter price of set card: ')
-        enteredPrice = input()
-        if enteredPrice != '-1':
-            allCardPrices.append(int(enteredPrice))
-    for i in allCardPrices:
-        totalSetCost += i
-    print('What is the card going for?')
-    buyPrice = input()
-    afterTaxPrice = int(buyPrice) * .9
-    profit = afterTaxPrice - totalSetCost
-    print(f'The set has a net of {profit} coins.')
+def initializeTool(prices, overall_range):
+    print('Let\'s add a new overall range.')
+    sell_price = input('What price can you find those at? ')
+    buy_price = float(sell_price) * .8
+    print(f'Let\'s start buying at {buy_price}.')
+    prices[f'{overall_range}'] = {
+        'Sell Price': sell_price,
+        'Buy Price': buy_price
+    }
+    return prices
+
+def salesReport():
+    sell_attempts = input('How many cards did you try to sell? ')
+    successful_sales = input('How many cards succesfully sold? ')
+    sell_percentage = (int(successful_sales) / int(sell_attempts)) * 100
+    return sell_percentage
+
+def priceAdjust(sell_percentage, overall_range, prices):
+    if sell_percentage <= 30:
+        print('That wasn\'t a good hour... lets lower the prices')
+        prices[f'{overall_range}']['Sell Price'] = float(prices[f'{overall_range}']['Sell Price']) * .98
+        prices[f'{overall_range}']['Buy Price'] = float(prices[f'{overall_range}']['Sell Price']) * .8
+    elif sell_percentage <= 50:
+        print('We could do better, let\'s lower by a bit.')
+        prices[f'{overall_range}']['Sell Price'] = float(prices[f'{overall_range}']['Sell Price']) * .99
+        prices[f'{overall_range}']['Buy Price'] = float(prices[f'{overall_range}']['Sell Price']) * .8
+    elif sell_percentage <= 70:
+        print('That was optimal, lets try maintaining this result.')
+    elif sell_percentage <= 100:
+        print('That was too good... let\'s try to raise the prices a bit.')
+        prices[f'{overall_range}']['Sell Price'] = float(prices[f'{overall_range}']['Sell Price']) * 1.02
+        prices[f'{overall_range}']['Buy Price'] = float(prices[f'{overall_range}']['Sell Price']) * .8
+    return prices
+
+if Path('./Config Settings.txt').is_file():
+    file = open('Config Settings.txt')
+    config_settings = []
+    for line in file:
+        config_settings.append(line[:-1])
+    print(config_settings)
+else:
+    config_settings = ['Initialized: 0']
+if config_settings[0][-1:] == '0':
+    prices = {}
+    print('Welcome to the Madden AH Tool. Let\'s begin with some basics.')
+    overall_range = input('What overall\'s are you viewing? ')
+    prices = initializeTool(prices, overall_range)
+    sell_percentage = salesReport()
+    prices = priceAdjust(sell_percentage, overall_range, prices)
+    print('Relist cards at: ' + str(prices[f'{overall_range}']['Sell Price']))
+    print('Buy new cards at: ' + str(prices[f'{overall_range}']['Buy Price']))
+    config_settings.append(f'{overall_range}')
+    config_settings.append(prices[f'{overall_range}']['Sell Price'])
+    config_settings.append(prices[f'{overall_range}']['Buy Price'])
+    config_settings[0] = 'Initialized: 1'
+else:
+    prices = {}
+    print('Welcome back to the Madden AH Tool.')
+    overall_range = input('What overall\'s are you viewing? ')
+    prices[f'{overall_range}'] = {}
+    for x in range(len(config_settings)):
+        if config_settings[x] == overall_range:
+            priceIndex = x + 1
+            prices[f'{overall_range}']['Sell Price'] = config_settings[priceIndex]
+            prices[f'{overall_range}']['Buy Price'] = config_settings[priceIndex+1]
+            overall_found = True
+            break
+        else:
+            overall_found = False
+    if not overall_found:
+        prices = initializeTool(prices, overall_range)
+        sell_percentage = salesReport()
+        prices = priceAdjust(sell_percentage, overall_range, prices)
+        print('Relist cards at: ' + str(prices[f'{overall_range}']['Sell Price']))
+        print('Buy new cards at: ' + str(prices[f'{overall_range}']['Buy Price']))
+        config_settings.append(f'{overall_range}')
+        config_settings.append(prices[f'{overall_range}']['Sell Price'])
+        config_settings.append(prices[f'{overall_range}']['Buy Price'])
+    else:
+        print('Relist cards at: ' + str(prices[f'{overall_range}']['Sell Price']))
+        print('Buy new cards at: ' + str(prices[f'{overall_range}']['Buy Price']))
+        sell_percentage = salesReport()
+        prices = priceAdjust(sell_percentage, overall_range, prices)
+        config_settings[priceIndex] = prices[f'{overall_range}']['Sell Price']
+        config_settings[priceIndex+1] = prices[f'{overall_range}']['Buy Price']
+        print('Relist cards at: ' + str(prices[f'{overall_range}']['Sell Price']))
+        print('Buy new cards at: ' + str(prices[f'{overall_range}']['Buy Price']))
+
+file = open('Config Settings.txt', 'w')
+for setting in config_settings:
+    file.write(str(setting) + '\n')
+file.close()
